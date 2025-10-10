c99c6e2f204875870d3806c6c23dab159dad6466
mfirmnba <mohammadfirmanbahriarofah@gmail.com>
2025-10-10 13:01:12 +0700

    mencoba react

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,10 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Dashboard - Owner</title>
-        @vite('resources/css/app.css')
+        @viteReactRefresh
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
         <script src="//unpkg.com/alpinejs" defer></script>
     </head>
     <style>
