8ee197ea26ac6c7e4a7fcb6c8a0d697d0f955095
mfirmnba <mohammadfirmanbahriarofah@gmail.com>
2025-10-10 13:30:52 +0700

    mencoba react

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
-        react(),
         tailwindcss(),
     ],
+    build: {
+        outDir: 'public/build', // 🟢 pastikan baris ini ada
+        manifest: true,
+        emptyOutDir: true,
+    },
+    server: {
+        cors: true,
+    },
 });
