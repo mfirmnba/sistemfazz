3e55b5549e15f08ef889f43d190eddbd3b76e386
mfirmnba <mohammadfirmanbahriarofah@gmail.com>
2025-10-13 23:49:50 +0700

    ganti config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            publicDirectory: 'public', // <-- penting
         }),
         tailwindcss(),
     ],
     build: {
-        outDir: 'public/build',
-        manifest: true,
+        outDir: 'public/build',  // hasil build ada di public/build
         emptyOutDir: true,
-    },
-    server: {
-        cors: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash].[ext]',
+            },
+        },
     },
 });
