fb3a643f1deb116c3c8549abadb1978fe5eaf07a
mfirmnba <mohammadfirmanbahriarofah@gmail.com>
2025-10-14 00:36:05 +0700

    done

--- resources/views/owner/dashboard.blade.php
+++ resources/views/owner/dashboard.blade.php
@@ -644,8 +644,6 @@
 </div>
 
 <!-- Chart.js -->
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns"></script>
 <script>
                                 // =========================
                                 // 1. Bar Chart: Bahan Terpakai Hari Ini
