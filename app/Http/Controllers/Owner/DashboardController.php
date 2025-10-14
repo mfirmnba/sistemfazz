8edd8e3ab45d6160ef069c5b6fd2bb09f958dd06
mfirmnba <mohammadfirmanbahriarofah@gmail.com>
2025-10-15 06:19:45 +0700

    nambah sikit eror

--- app/Http/Controllers/Owner/DashboardController.php
+++ app/Http/Controllers/Owner/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Owner;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Models\Minuman;
 use App\Models\LaporanProduksiHistory;
