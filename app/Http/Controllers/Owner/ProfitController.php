e56ad517772b50c2426ff40add3f404e79ac118f
PT-Kinian-Inspirasi-Indonesia <kiniancoffee@gmail.com>
2025-11-07 20:10:24 +0700

    profitcontroller

--- app/Http/Controllers/Owner/ProfitController.php
+++ app/Http/Controllers/Owner/ProfitController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use App\Models\LaporanPenjualan;
-use App\Models\Minuman;
 
 class ProfitController extends Controller
 {
@@ -16,20 +15,10 @@ class ProfitController extends Controller
             ->get()
             ->sum(fn($item) => (($item->minuman->harga ?? 0) - ($item->minuman->hpp ?? 0)) * ($item->jumlah ?? 0));
 
-        // Kamu juga bisa tambahkan data lain kalau perlu
+        // Kalau kamu ingin tampilkan juga total penjualan
         $totalPenjualan = LaporanPenjualan::where('status', 'terjual')->sum('jumlah');
-        $totalKeuntunganSemua = LaporanPenjualan::with('minuman')
-            ->where('status', 'terjual')
-            ->get()
-            ->sum(fn($item) => (($item->minuman->harga ?? 0) - ($item->minuman->hpp ?? 0)) * ($item->jumlah ?? 0));
 
-        $profitPerMinuman = LaporanPenjualan::join('minumans', 'laporan_penjualans.minuman_id', '=', 'minumans.id')
-            ->where('laporan_penjualans.status', 'terjual')
-            ->selectRaw('minumans.nama, SUM(laporan_penjualans.jumlah) as total_cup, SUM((minumans.harga - minumans.hpp) * laporan_penjualans.jumlah) as total_profit')
-            ->groupBy('minumans.nama')
-            ->orderByDesc('total_profit')
-            ->get();
-
-        return view('owner.laporan.profit', compact('totalKeuntunganSemua', 'profitPerMinuman'));
+        // Kirim ke view
+        return view('owner.laporan.profit', compact('totalProfit', 'totalPenjualan'));
     }
 }
